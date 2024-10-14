952661ba78378e923669a2acda3c048174b3e74b
Владислав <vladvarlakov498@gmail.com>
2024-10-14 23:22:01 +0500

    Reworked Game_Manager script for singleton
    Переписал скрипт Game_manager на паттерн Singleton

--- Assets/Scripts/Game_manager.cs
+++ Assets/Scripts/Game_manager.cs
@@ -1,8 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Game_manager : MonoBehaviour
 {
+    private static Game_manager _instance;
+
+    public static Game_manager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<Game_manager>();
+                if (_instance == null)
+                {
+                    GameObject go = new GameObject();
+                    go.name = "Game_manager";
+                    _instance = go.AddComponent<Game_manager>();
+                }
+            }
+            return _instance;
+        }
+    }
+
     public int coinsCount = 0;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
 }
