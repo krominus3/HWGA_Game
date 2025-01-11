6acfbc6a300c30b9d5de9a0b7755f1dd887184e3
krominus3 <krominus3@gmail.com>
2025-01-11 18:58:00 +0500

    bug fix

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -7,8 +7,8 @@ public class TimeManager : MonoBehaviour
 {
     [SerializeField] private Text timer;
 
-    private const float baseLifeTime = 1f;
-    //private const float baseLifeTime = -10f;
+    //private const float baseLifeTime = 1f;
+    private const float baseLifeTime = -10f;
 
     [SerializeField] public float lifeTime;
 
